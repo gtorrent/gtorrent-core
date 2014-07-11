fef0de0c476b143d6eb7572702b3823e1b32b9bb
fatum <anon@anon.com>
2014-07-11 10:51:01 -0500

    demonostration of accessing the core

--- src/lib/gui/gtk/Gtk.cpp
+++ src/lib/gui/gtk/Gtk.cpp
@@ -13,6 +13,8 @@ GuiGtk::GuiGtk(int argc, char **argv)
 
 	Glib::RefPtr<Gtk::Application> gapp = Gtk::Application::create(argc, argv, "org.gtorrent.gtorrent");
 	GtkMainWindow *wnd = new GtkMainWindow();
+
+	printf("\nFile #1 (Read From GUI) : %s\n", m_core->getEngine()->getTorrents()[0]->getPath().c_str());
 }
 
 void GuiGtk::update()
