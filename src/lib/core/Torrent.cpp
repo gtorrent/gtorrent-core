db1ee52cf1309c238b2523648188cd60e8dc64e7
= <gtorrent.gentoo@gmail.com>
2014-07-12 03:00:09 -0500

    Added new data quering options.
     - Total Percent
     - Total Seeders
     - Total Peers

--- src/lib/core/Torrent.cpp
+++ src/lib/core/Torrent.cpp
@@ -1,6 +1,6 @@
 #include "Torrent.hpp"
 
-#define T_PPM 1000000
+#define T_PPM 1000000.f
 
 Torrent::Torrent(std::string path) :
 	m_path(path)
@@ -25,16 +25,38 @@ libtorrent::add_torrent_params Torrent::getTorrentParams()
 	return m_torrent_params;
 }
 
+libtorrent::torrent_handle &Torrent::getHandle()
+{
+	return m_handle;
+}
+
 std::string Torrent::getPath()
 {
 	return m_path;
 }
 
-unsigned int Torrent::getTotalProgress()
+float Torrent::getTotalProgress()
 {
 	libtorrent::torrent_status s = m_handle.status();
 
-	return (s.progress_ppm / T_PPM) * 100;
+	return ((float) s.progress_ppm / (float) T_PPM) * 100;
+}
+
+unsigned int Torrent::getPPMProgress()
+{
+	libtorrent::torrent_status s = m_handle.status();
+
+	return s.progress_ppm;
+}
+
+unsigned int Torrent::getTotalSeeders()
+{
+	return m_handle.status().num_seeds;
+}
+
+unsigned int Torrent::getTotalPeers()
+{
+	return m_handle.status().num_peers;
 }
 
 void Torrent::setHandle(libtorrent::torrent_handle &h)
--- src/lib/core/Torrent.hpp
+++ src/lib/core/Torrent.hpp
@@ -4,6 +4,8 @@
 #include "Event.hpp"
 #include <memory>
 
+class Torrent;	typedef std::shared_ptr<Torrent> t_ptr;
+
 class Torrent
 {
 private:
@@ -11,6 +13,7 @@ private:
 	libtorrent::add_torrent_params m_torrent_params;
 	libtorrent::torrent_handle m_handle;
 	std::string m_path;
+	
 public:
 	Torrent(std::string path);
 
@@ -18,10 +21,20 @@ public:
 
 	/* Getters */
 	libtorrent::add_torrent_params getTorrentParams();
+	libtorrent::torrent_handle &getHandle();
 	std::string getPath();
 	
 	/* Returns percentage of all files downloading */
-	unsigned int getTotalProgress();
+	float getTotalProgress();
+
+	/* Returns the progress in PPM of all files downloading */
+	unsigned int getPPMProgress();
+
+	/* Returns the current number of seeders attached to the file */
+	unsigned int getTotalSeeders();
+
+	/* Returns the current number of peers attached to the file */
+	unsigned int getTotalPeers();
 
 	/* Setters */
 	void setHandle(libtorrent::torrent_handle &h);
