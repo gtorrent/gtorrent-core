7f83d9be94d1796576fe280170697813d7fb46dd
= <gtorrent.gentoo@gmail.com>
2014-07-13 16:55:32 -0500

    Removed useless OSHelper class

--- src/lib/core/OSHelper.cpp
+++
@@ -1,7 +0,0 @@
-#include "OSHelper.hpp"
-
-std::string OSHelper::getBasename(std::string strPath)
-{
-	size_t iLastSeparator = 0;
-    return strPath.substr((iLastSeparator = strPath.find_last_of("\\")) != std::string::npos ? iLastSeparator + 1 : 0, strPath.size() - strPath.find_last_of("."));
-}
