c05f937282f353e2d8dc423186e56ae9d89d3b25
= <gtorrent.gentoo@gmail.com>
2014-07-13 04:58:41 -0500

    Removed "bloat"

--- src/gui/gtk/GtkTorrentExplorerWindow.cpp
+++
@@ -1,9 +0,0 @@
-#include "GtkTorrentExplorerWindow.hpp"
-#include <gtkmm/main.h>
-
-GtkTorrentExplorerWindow::GtkTorrentExplorerWindow()
-{
-	this->set_position(Gtk::WIN_POS_CENTER);
-	this->set_default_size(900, 600);
-	this->show_all();
-}
--- src/gui/gtk/GtkWindow.cpp
+++
@@ -1,7 +0,0 @@
-#include <Application.hpp>
-#include "GtkWindow.hpp"
-
-gt::GtkWindow::GtkWindow() :
-	m_core(Application::getSingleton()->getCore())
-{
-}
