69d5148567946bc5fb2ce93c8ed15146072f5687
Ben Iofel <iofelben@gmail.com>
2014-07-15 11:53:39 -0400

    added travis check

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,6 +1,8 @@
-sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-add-repository -y ppa:gnome3-team/gnome3
-sudo apt-add-repository -y ppa:gnome3-team/gnome3-staging
-sudo apt-add-repository -y ppa:ricotz/testing
-sudo apt-get update -q
-sudo apt-get install -qy cmake libgtkmm-3.0-dev libglibmm-2.4-dev libtorrent-rasterbar-dev libboost-dev g++-4.8
+if [[ $(whoami) = "travis" ]]; then
+	sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
+	sudo apt-add-repository -y ppa:gnome3-team/gnome3
+	sudo apt-add-repository -y ppa:gnome3-team/gnome3-staging
+	sudo apt-add-repository -y ppa:ricotz/testing
+	sudo apt-get update -q
+	sudo apt-get install -qy cmake libgtkmm-3.0-dev libglibmm-2.4-dev libtorrent-rasterbar-dev libboost-dev g++-4.8
+fi
