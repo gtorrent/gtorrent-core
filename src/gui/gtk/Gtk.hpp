1c92d2bfa5da267f9ad360fdf1196e6e8a39ca7e
truNEET <gtorrent.gentoo@gmail.com>
2014-07-15 02:06:52 -0500

    Destruct mainWindow pointer on gTorrent shutdown

--- src/gui/gtk/Gtk.hpp
+++ src/gui/gtk/Gtk.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gui/core/Base.hpp>
+#include "GtkMainWindow.hpp"
 #include <gtkmm/settings.h>
 #include <gtkmm/main.h>
 #include <memory>
@@ -9,9 +10,10 @@ class GuiGtk : public GuiBase
 {
 private:
 	Gtk::Main *m_gtkmain;
-
+	GtkMainWindow *mainWindow;
 public:
 	GuiGtk(int argc, char **argv);
+	~GuiGtk();
 
 	// Public Members & Functions
 
