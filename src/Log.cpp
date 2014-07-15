16afa197d8161fd5bea61d16babec0e5fe7555e2
truNEET <gtorrent.gentoo@gmail.com>
2014-07-15 02:07:49 -0500

    Add actual timestamp to log function

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,10 +1,49 @@
 #include "Log.hpp"
+#include <sstream>
 #include <cstdio>
+#include <ctime>
+
+std::string getFullDateNumber(int n)
+{
+	std::ostringstream oss;
+
+	if (n < 10) {
+		oss << "0" << n;
+	} else {
+		oss << n;
+	}
+
+	return oss.str();
+}
+
+std::string gt::Log::getTimeStamp()
+{
+	time_t t = time(0);
+	struct tm *lt = localtime(&t);
+
+	// TODO: Maybe convert this stream string structure into something else.
+
+	std::stringstream oss;
+
+	oss << (1900 + lt->tm_year);
+	oss << "-";
+	oss << getFullDateNumber(lt->tm_mon);
+	oss << "-";
+	oss << getFullDateNumber(lt->tm_mday);
+	oss << " @ ";
+	oss << getFullDateNumber(lt->tm_hour);
+	oss << ":";
+	oss << getFullDateNumber(lt->tm_min);
+	oss << ":";
+	oss << getFullDateNumber(lt->tm_sec);
+
+	return oss.str();
+}
 
 void gt::Log::Debug(const char *fmt, ...)
 {
 	va_list args;
 
-	printf("[TIME]: ");
+	printf("[%s]: ", gt::Log::getTimeStamp().c_str());
 	vprintf(fmt, args);
 }
--- src/core/Log.hpp
+++ src/core/Log.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <string>
+
 namespace gt
 {
 	class Log
 	{
 	public:
+		static std::string getTimeStamp();
 		static void Debug(const char *fmt, ...);
 	};
 };
