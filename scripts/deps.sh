35dd2d3c4f94adbe940436e59b2fa843e7534c62
benwaffle <iofelben@gmail.com>
2014-07-15 11:48:24 -0400

    try now?

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,27 +1,5 @@
-# glibmm
-wget http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.40/glibmm-2.40.0.tar.xz
-tar xf glibmm-2.40.0.tar.xz
-cd glibmm-2.40.0
-./configure
-make
-sudo make install
-cd ..
-
-# gtk+
-wget http://ftp.gnome.org/pub/gnome/sources/gtk+/3.12/gtk+-3.12.2.tar.xz
-tar xf gtk+-3.12.2.tar.xz
-cd gtk+-3.12.2
-./configure
-make
-sudo make install
-cd ..
-
-# gtkmm
-wget http://ftp.gnome.org/pub/GNOME/sources/gtkmm/3.12/gtkmm-3.12.0.tar.xz
-tar xf gtkmm-3.12.0.tar.xz
-cd gtkmm-3.12.0
-./configure
-make
-sudo make install
-cd ..
-
+sudo apt-add-repository -y ppa:gnome3-team/gnome3
+sudo apt-add-repository -y ppa:gnome3-team/gnome3-staging
+sudo apt-add-repository -y ppa:ricotz/testing
+sudo apt-get update -q
+sudo apt-get install -qy cmake libgtkmm-3.0-dev libglibmm-2.4-dev libtorrent-rasterbar-dev libboost-dev g++-4.8
