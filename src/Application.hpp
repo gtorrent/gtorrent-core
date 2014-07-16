7cda50d646e7b04db04d247a42629a8bba8f5b66
truNEET <gtorrent.gentoo@gmail.com>
2014-07-15 19:52:47 -0500

    Remove typedef from Application duplicate definition.

--- src/Application.hpp
+++ src/Application.hpp
@@ -3,24 +3,18 @@
 #include <gui/core/Base.hpp>
 #include <gui/gtk/Gtk.hpp>
 #include <core/Core.hpp>
-#include <memory>
-
-class Application;
-
-typedef std::shared_ptr<Application> app_ptr;
 
 class Application
 {
 private:
-	static app_ptr m_app;
+	static shared_ptr<Application> m_app;
 	
 	// Private Member Functions & Variables 
 	core_ptr m_core;
 	gui_ptr m_gui;
 
 public:
-	Application();
-	static app_ptr getSingleton();
+	static shared_ptr<Application> getSingleton();
 
 	// Getters
 	core_ptr &getCore();
