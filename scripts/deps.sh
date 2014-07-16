e9c847f024e61aae7c73a24e6d11bcd2806b49a2
Michael Dec <grepwood@sucs.org>
2014-07-16 04:20:57 +0100

    added root check
    so we don't need to call sudo from inside of the script

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 SATISFIED=0
-if [ -f "/etc/debian_version" ]; then
-  sudo apt-get install -y cmake libgtkmm-3.0-dev libglibmm-2.4-dev libtorrent-rasterbar-dev libboost-all-dev libssl-dev
-  SATISFIED=1
-fi
+USER=`whoami`
+if [ "$USER" != "root" ]; then
+  echo "You have to sudo or be root to install dependencies."
+else
+  if [ -f "/etc/debian_version" ]; then
+    apt-get install -y cmake libgtkmm-3.0-dev libglibmm-2.4-dev libtorrent-rasterbar-dev libboost-all-dev libssl-dev
+    SATISFIED=1
+  fi
 
-if [ -f "/etc/gentoo-release" ]; then
-  emerge -vq dev-util/cmake dev-cpp/gtkmm dev-cpp/glibmm net-libs/rb_libtorrent dev-libs/boost dev-libs/openssl
-  SATISFIED=1
-fi
+  if [ -f "/etc/gentoo-release" ]; then
+    emerge -vq dev-util/cmake dev-cpp/gtkmm dev-cpp/glibmm net-libs/rb_libtorrent dev-libs/boost dev-libs/openssl
+    SATISFIED=1
+  fi
 
-if [ "$SATISFIED" -ne "1" ]; then
-  echo "Your system isn't supported yet."
+  if [ "$SATISFIED" -ne "1" ]; then
+    echo "Your system isn't supported yet."
+  fi
 fi
