5db6a9bad2ca5341d86e58d7c5118c3960a66189
shinigamichan <shinigami@cock.li>
2014-07-15 21:30:37 -0700

    Allows compatability with both libtorrent 1.0 and svn (1.1) rectified with construct syntax required in boost 1.55.

--- src/Torrent.cpp
+++ src/Torrent.cpp
@@ -1,22 +1,21 @@
 #include <core/Core.hpp>
 #include "Torrent.hpp"
-
 #define T_PPM 1000000.f
-
 Torrent::Torrent(string path) :
 	m_path(path)
 {
 	m_torrent_params.save_path = "./";
-
 	if (gt::Core::isMagnetLink(path))
 	{
 		m_torrent_params.url = path;
 	}
 	else
 	{
-			m_torrent_params.ti = boost::intrusive_ptr<libtorrent::torrent_info>(new libtorrent::torrent_info(path)); // This is the proper way to cast a pointer to an intrusive_ptr
+			//libtorrent::add_torrent_params.ti is an intrusive_ptr in 1.0 and a shared_ptr in svn.
+			//Using decltype allows us to make it compatible with both versions while still properly using the constructor to avoid a compiler error on boost 1.55 when the = operator is used with a pointer.
+			m_torrent_params.ti = decltype(m_torrent_params.ti)(new libtorrent::torrent_info(path)); 
 	}
-		
+
 }
 
 bool Torrent::pollEvent(gt::Event &event)
