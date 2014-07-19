be86390d5902b6a4d7d3f3fc2cb9dff6a223db1e
mirai <mirai@localhost.localdomain>
2014-07-19 15:38:00 +0200

    Adds working context menu to hide columns.

--- src/gui/gtk/GtkTorrentTreeView.cpp
+++ src/gui/gtk/GtkTorrentTreeView.cpp
@@ -9,7 +9,6 @@ GtkTorrentTreeView::GtkTorrentTreeView()
 	m_liststore = Gtk::ListStore::create(m_cols);
 	this->set_model(m_liststore);
 	this->setupColumns();
-
 	signal_button_press_event().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::view_onClick), false);
 }
 
@@ -28,25 +27,30 @@ bool GtkTorrentTreeView::view_onClick(GdkEventButton *event)
 	return false;
 }
 
-void GtkTorrentTreeView::torrentContext_onClick()
-{
-	puts(">2014\n>touching my tralala");
-}
-
-
-void GtkTorrentTreeView::columnContext_onClick()
-{
-	puts(">2014\n>touching my dingdingdong");
-}
-
 bool GtkTorrentTreeView::torrentColumns_onClick(GdkEventButton *event)
 {
 	if(event->button == 3) 
 	{
 		Gtk::Menu     *rcMenu  = Gtk::manage(new Gtk::Menu());
-		Gtk::MenuItem *rcmItem = Gtk::manage(new Gtk::MenuItem("This is a click on a torrent column"));
-		rcmItem->signal_activate().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::columnContext_onClick));
-		rcMenu->add(*rcmItem);
+
+		Gtk::MenuItem *rcmItem1 = Gtk::manage(new Gtk::MenuItem("Name"));
+		Gtk::MenuItem *rcmItem2 = Gtk::manage(new Gtk::MenuItem("Seeders"));
+		Gtk::MenuItem *rcmItem3 = Gtk::manage(new Gtk::MenuItem("Leechers"));
+		Gtk::MenuItem *rcmItem4 = Gtk::manage(new Gtk::MenuItem("Rate"));
+		Gtk::MenuItem *rcmItem5 = Gtk::manage(new Gtk::MenuItem("Progress"));
+
+		rcmItem1->signal_activate().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::    nameContext_onClick));
+		rcmItem4->signal_activate().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::    rateContext_onClick));
+		rcmItem2->signal_activate().connect(sigc::mem_fun(*this, &GtkTorrentTreeView:: seedersContext_onClick));
+		rcmItem3->signal_activate().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::leechersContext_onClick));
+		rcmItem5->signal_activate().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::progressContext_onClick));
+
+		rcMenu->add(*rcmItem1);
+		rcMenu->add(*rcmItem2);
+		rcMenu->add(*rcmItem3);
+		rcMenu->add(*rcmItem4);
+		rcMenu->add(*rcmItem5);
+
 		rcMenu->show_all();
 		rcMenu->popup(event->button, event->time);
 	}
@@ -58,32 +62,43 @@ void GtkTorrentTreeView::setupColumns()
 {
 	unsigned int cid = 0;
 	Gtk::TreeViewColumn *col = nullptr;
-
-	cid = this->append_column("Name", m_cols.m_col_name);
-	col = this->get_column(cid - 1);
-	col->set_fixed_width(250);
-
-	cid = this->append_column("Seeders", m_cols.m_col_seeders);
-	col = this->get_column(cid - 1);
-	col->set_alignment(0.5);
-	col->set_fixed_width(90);
-	
-	cid = this->append_column("Leechers", m_cols.m_col_leechers);
-	col = this->get_column(cid - 1);
-	col->set_alignment(0.5);
-	col->set_fixed_width(90);
-
-	cid = this->append_column("Rate", m_cols.m_col_dl_speed);
-	col = this->get_column(cid - 1);
-	col->set_alignment(0.5);
-	col->set_fixed_width(95);
-
-	Gtk::CellRendererProgress *cell = Gtk::manage(new Gtk::CellRendererProgress());
-	cid = this->append_column("Progress", *cell);
-	col = this->get_column(cid - 1);
-
-	if (col) 
+	Gtk::CellRendererProgress *cell = nullptr;
+	if(visibleColumns & 1)
 	{
+		cid = this->append_column("Name", m_cols.m_col_name);
+		col = this->get_column(cid - 1);
+		col->set_fixed_width(250);
+	}
+
+	if(visibleColumns & 2)
+	{
+		cid = this->append_column("Seeders", m_cols.m_col_seeders);
+		col = this->get_column(cid - 1);
+		col->set_alignment(0.5);
+		col->set_fixed_width(90);
+	}
+
+	if(visibleColumns & 4)
+	{
+		cid = this->append_column("Leechers", m_cols.m_col_leechers);
+		col = this->get_column(cid - 1);
+		col->set_alignment(0.5);
+		col->set_fixed_width(90);
+	}
+
+	if(visibleColumns & 8)
+	{
+		cid = this->append_column("Rate", m_cols.m_col_dl_speed);
+		col = this->get_column(cid - 1);
+		col->set_alignment(0.5);
+		col->set_fixed_width(95);
+	}
+
+	if(visibleColumns & 16)
+	{
+		cell = Gtk::manage(new Gtk::CellRendererProgress());
+		cid = this->append_column("Progress", *cell);
+		col = this->get_column(cid - 1);
 		col->add_attribute(cell->property_value(), m_cols.m_col_percent);
 		col->add_attribute(cell->property_text(), m_cols.m_col_percent_text);
 	}
@@ -91,6 +106,7 @@ void GtkTorrentTreeView::setupColumns()
 	for (auto &c : this->get_columns()) 
 	{
 		c->set_sizing(Gtk::TreeViewColumnSizing::TREE_VIEW_COLUMN_FIXED);
+
 		Gtk::Button *butt = c->get_button();	
 		butt->signal_button_press_event().connect(sigc::mem_fun(*this, &GtkTorrentTreeView::torrentColumns_onClick));
 
@@ -98,6 +114,7 @@ void GtkTorrentTreeView::setupColumns()
 		c->set_resizable();
 		c->set_reorderable();
 	}
+
 
 }
 
@@ -112,13 +129,14 @@ void GtkTorrentTreeView::addCell(shared_ptr<Torrent> &t)
 	row[m_cols.m_col_percent_text] = t->getTextState();
 	row[m_cols.m_col_seeders] = t->getTotalSeeders();
 	row[m_cols.m_col_leechers] = t->getTotalLeechers();
+
 }
 
 void GtkTorrentTreeView::updateCells()
 {
 	unsigned int i = 0;
-
-	for (auto &c : m_liststore->children()) {
+	for (auto &c : m_liststore->children())
+	{
 		shared_ptr<Torrent> t = Application::getSingleton()->getCore()->getTorrents()[i];
 
 		c[m_cols.m_col_percent] = t->getTotalProgress();
@@ -135,3 +153,44 @@ void GtkTorrentTreeView::updateCells()
 	}
 }
 
+void GtkTorrentTreeView::torrentContext_onClick()
+{
+//	insert_column(m_cols.m_col_percent, 4);
+}
+
+
+void GtkTorrentTreeView::nameContext_onClick()
+{
+	visibleColumns ^= 1;
+	Rebuild();
+}
+
+void GtkTorrentTreeView::seedersContext_onClick()
+{
+	visibleColumns ^= 2;
+	Rebuild();
+}
+
+void GtkTorrentTreeView::leechersContext_onClick()
+{
+	visibleColumns ^= 4;
+	Rebuild();
+}
+
+void GtkTorrentTreeView::rateContext_onClick()
+{
+	visibleColumns ^= 8;
+	Rebuild();
+}
+
+void GtkTorrentTreeView::progressContext_onClick()
+{
+	visibleColumns ^= 16;
+	Rebuild();
+}
+
+void GtkTorrentTreeView::Rebuild()
+{
+	remove_all_columns();
+	setupColumns();
+}
--- src/gui/gtk/GtkTorrentTreeView.hpp
+++ src/gui/gtk/GtkTorrentTreeView.hpp
@@ -11,22 +11,22 @@
 class GtkTorrentColumns : public Gtk::TreeModel::ColumnRecord
 {
 public:
-	GtkTorrentColumns()
+	GtkTorrentColumns(unsigned int flags = 0xFFFFFFFF)
 	{
-		add(m_col_name);
-		add(m_col_seeders);
-		add(m_col_leechers);
-		add(m_col_dl_speed);
-		add(m_col_percent);
-		add(m_col_percent_text);
+		if(flags &  1) add(m_col_name);
+		if(flags &  2) add(m_col_seeders);
+		if(flags &  4) add(m_col_leechers);
+		if(flags &  8) add(m_col_dl_speed);
+		if(flags & 16) add(m_col_percent);
+		if(flags & 32) add(m_col_percent_text);
 	}
 	
 	Gtk::TreeModelColumn<Glib::ustring> m_col_name;
-	Gtk::TreeModelColumn<unsigned int> m_col_seeders;
-	Gtk::TreeModelColumn<unsigned int> m_col_leechers;
-	Gtk::TreeModelColumn<unsigned int> m_col_percent;
+	Gtk::TreeModelColumn<unsigned int>  m_col_seeders;
+	Gtk::TreeModelColumn<unsigned int>  m_col_leechers;
+	Gtk::TreeModelColumn<unsigned int>  m_col_percent;
 	Gtk::TreeModelColumn<Glib::ustring> m_col_percent_text;
-	Gtk::TreeModelColumn<unsigned int> m_col_empty;
+	Gtk::TreeModelColumn<unsigned int>  m_col_empty;
 	Gtk::TreeModelColumn<Glib::ustring> m_col_dl_speed;
 };
 
@@ -37,12 +37,19 @@ class GtkTorrentTreeView : public Gtk::TreeView
 private:
 	GtkTorrentColumns m_cols;
 	Glib::RefPtr<Gtk::ListStore> m_liststore;
+	unsigned visibleColumns = 0xFFFFFFFF;
+
 	void setupColumns();
 
-	bool           view_onClick(GdkEventButton *event);
-	bool torrentColumns_onClick(GdkEventButton *event);
-	void           torrentContext_onClick();
-	void           columnContext_onClick();
+	bool                      view_onClick(GdkEventButton *event);
+	bool            torrentColumns_onClick(GdkEventButton *event);
+	void            torrentContext_onClick();
+	void               nameContext_onClick();
+	void            seedersContext_onClick();
+	void           leechersContext_onClick();
+	void               rateContext_onClick();
+	void           progressContext_onClick();
+	void Rebuild();
 	
 public:
 	GtkTorrentTreeView();
