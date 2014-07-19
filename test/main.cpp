83da2dad6778a0f7237a0ac2c81a7c2c3910b97f
ragesalmon <ragesalmon@gmail.com>
2014-07-19 15:41:57 -0400

    Adding test directory

---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+#include <gtorrent/libtorrent.hpp>
+#include <gtorrent/Core.hpp>
+#include <gtorrent/Event.hpp>
+#include <gtorrent/Log.hpp>
+#include <gtorrent/Torrent.hpp>
+
+int main(int argc, char *argv[]) {
+	printf("gTest.exe started");
+	gt::Log::Debug("Testing logging");
+	return 0;
+}
+
