e3ac0697cfba8d5756e7feba5c2543bde7fbab1c
truNEET <gtorrent.gentoo@gmail.com>
2014-07-18 19:18:19 -0500

    Update Torrent namespace

--- include/gtorrent/Core.hpp
+++ include/gtorrent/Core.hpp
@@ -2,28 +2,21 @@
 
 #include "libtorrent.hpp"
 #include "Torrent.hpp"
-#include <memory>
-#include <string>
 
 namespace gt
 {
 	class Core
 	{
 	private:
-		vector<shared_ptr<Torrent> > m_torrents;
+		vector<shared_ptr<gt::Torrent> > m_torrents;
 		libtorrent::session m_session;
-		bool m_running;
 	public:
 		Core();
 
 		static bool isMagnetLink(string const& link);
 		
 		static string getDefaultSavePath();
-		vector<shared_ptr<Torrent> > &getTorrents();
-		shared_ptr<Torrent> addTorrent(string path);
-		
-		bool isRunning();
-		void shutdown();
-		void update();
+		vector<shared_ptr<gt::Torrent> > &getTorrents();
+		shared_ptr<gt::Torrent> addTorrent(string path);
 	};
 }
