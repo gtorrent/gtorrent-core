a2c1314c356ddf7432755487f7546730098c071a
truNEET <gtorrent.gentoo@gmail.com>
2014-07-18 19:19:22 -0500

    Remove header files from source tree

--- src/libtorrent.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <libtorrent/torrent_info.hpp>
-#include <libtorrent/lazy_entry.hpp>
-#include <libtorrent/session.hpp>
-#include <libtorrent/bencode.hpp>
-#include <libtorrent/entry.hpp>
-
