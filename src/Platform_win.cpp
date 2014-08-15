c4e318de736ca5423bce12c6e0a9d9f8e307fbec
shinigamichan <shinigami@cock.li>
2014-08-14 21:41:25 -0700

    Fix problems caused by removal of using std.

--- src/Platform_win.cpp
+++ src/Platform_win.cpp
@@ -13,30 +13,30 @@
 
 // This will be renamed when Platform_linux.cpp and Platform.hpp change
 // Actually checks if file/directory exists.
-bool gt::Platform::checkDirExist(string file)
+bool gt::Platform::checkDirExist(std::string file)
 {
     DWORD f_attrib = GetFileAttributes(file.c_str());
     if(f_attrib != INVALID_FILE_ATTRIBUTES)
     {
-        gt::Log::Debug(string(file + " exists,").c_str());
+        gt::Log::Debug(std::string(file + " exists,").c_str());
         return true;
     }
     else
     {
-        gt::Log::Debug(string(file + " doesn't exist,").c_str());
+        gt::Log::Debug(std::string(file + " doesn't exist,").c_str());
         return false;
     }
 }
 
-string gt::Platform::getDefaultSavePath()
+std::string gt::Platform::getDefaultSavePath()
 {
-    gt::Log::Debug(string(getHomeDir() + "Downloads\\ is the default save path").c_str());
+    gt::Log::Debug(std::string(getHomeDir() + "Downloads\\ is the default save path").c_str());
     return getHomeDir() + "Downloads";
 }
 
-string gt::Platform::getDefaultConfigPath()
+std::string gt::Platform::getDefaultConfigPath()
 {
-    return string(getenv("APPDATA")) + "\\gtorrent\\";
+    return std::string(getenv("APPDATA")) + "\\gtorrent\\";
 }
 
 char gt::Platform::getFileSeparator()
@@ -44,29 +44,29 @@ char gt::Platform::getFileSeparator()
     return '\\';
 }
 
-string gt::Platform::getHomeDir()
+std::string gt::Platform::getHomeDir()
 {
-    return string(getenv("HOMEDRIVE")) + string(getenv("HOMEPATH")) + '\\';
+    return std::string(getenv("HOMEDRIVE")) + std::string(getenv("HOMEPATH")) + '\\';
 }
 
-string getTempDir()
+std::string getTempDir()
 {
-    return string(getenv("TMP")) + '\\';
+    return std::string(getenv("TMP")) + '\\';
 }
 
-int gt::Platform::makeDir(string dir, unsigned mode)
+int gt::Platform::makeDir(std::string dir, unsigned mode)
 {
     // TODO Actually use mode? Who really cares about perms in windows?
     return CreateDirectory(dir.c_str(), NULL);
 }
 
-string gt::Platform::getExecutablePath()
+std::string gt::Platform::getExecutablePath()
 {
     // A path is usually not 4096 chars long, right?
     char ExecutablePath[4096] = { 0 };
     GetModuleFileName(NULL, ExecutablePath, 4096);
 
-    return string(ExecutablePath);
+    return std::string(ExecutablePath);
 }
 
 void gt::Platform::associate(bool magnet, bool torrent)
@@ -157,10 +157,10 @@ void gt::Platform::disableSharedData()
 	DeleteFile(std::string(getDefaultConfigPath() + "gtorrent.lock").c_str());
 }
 
-void gt::Platform::openTorrent(shared_ptr<gt::Torrent> t)
+void gt::Platform::openTorrent(std::shared_ptr<gt::Torrent> t)
 {
     auto files = t->getInfo()->files();
-    string path = t->getSavePath() + '\\' + t->getInfo()->file_at(0).path;
+    std::string path = t->getSavePath() + '\\' + t->getInfo()->file_at(0).path;
 
     if(files.num_files() > 1)
         path = path.substr(0, path.find_last_of('\\'));
