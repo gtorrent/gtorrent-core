b367e54c332510beb4064f2af4a663e1405f4423
IGI-111 <igi.222@gmail.com>
2014-09-07 23:33:47 +0200

    add inlining instead of moving values around

--- include/gtorrent/Peer.hpp
+++ include/gtorrent/Peer.hpp
@@ -2,15 +2,26 @@
 
 #include <libtorrent/peer_info.hpp>
 
+#include "Torrent.hpp"
+
 namespace gt
 {
 	class Peer : public libtorrent::peer_info
 	{
 		public:
 			Peer(const libtorrent::peer_info &p);
-			std::string ip;
-			std::string uploadSpeed;
-			std::string downloadSpeed;
-			std::string client;
+			inline std::string getIP() const
+			{
+				return ip.address().to_string();
+			}
+			inline std::string getUploadSpeed() const
+			{
+				return std::to_string(up_speed);
+			}
+			inline std::string getDownloadSpeed() const
+			{
+				return std::to_string(down_speed);
+			}
+			std::string getClient() const;
 	};
 }
