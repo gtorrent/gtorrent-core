5ee560519a5d82f44cbaaa6c3e5b8fa96bb54491
infoburp <gwolfendale@gmail.com>
2014-09-08 21:33:57 +0100

    debug

--- src/Peer.cpp
+++ src/Peer.cpp
@@ -4,10 +4,6 @@
 
 
 gt::Peer::Peer(const libtorrent::peer_info &p) :
-	ip(p.ip.address().to_string()),
-	uploadSpeed(getRateString(p.up_speed)),
-	downloadSpeed(getRateString(p.down_speed)),
-	client(p.client),
 	peer_info(p)
 {
 }
